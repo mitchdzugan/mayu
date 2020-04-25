1af40468d250c1c8fe7c12c0291007f788fcb58f
Mitch Dzugan <mitchdzugan@gmail.com>
2020-04-24 20:31:09 -0500

    update deps

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject mayu "0.1.5"
+(defproject mayu "0.1.6"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [allpa "0.1.35"]
+                 [allpa "0.1.37"]
                  [wayra "0.0.23"]
                  [org.clojure/core.async "1.1.587"]
                  [net.cgrand/macrovich "0.2.1"]

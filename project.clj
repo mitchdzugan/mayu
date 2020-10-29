ad30b4ffba16a106c91d5c2732c890f2235830e0
mitchdzugan <mitchdzugan@gmail.com>
2020-10-29 09:55:57 -0500

    increment versions

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject mayu "0.1.47"
+(defproject mayu "0.1.48"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [allpa "0.1.44"]
-                 [wayra "0.0.29"]
+                 [allpa "0.1.45"]
+                 [wayra "0.0.31"]
                  [org.clojure/core.async "1.1.587"]
                  [net.cgrand/macrovich "0.2.1"]
                  [cljsjs/snabbdom "0.7.4-1"]
